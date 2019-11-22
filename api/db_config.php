d08ed8f757e5c92673b4fd8f04e3f0b7cdbe693d
Tetiana127 <tsmagulam@ukr.net>
2019-11-22 11:34:28 +0200

    pershiy commit

---
+++ api/db_config.php
@@ -0,0 +1,7 @@
+<?php
+	define ('DB_USER', "localhost");
+	define ('DB_PASSWORD', "root");
+	define ('DB_DATABASE', "");
+	define ('DB_HOST', "items");
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+?>
